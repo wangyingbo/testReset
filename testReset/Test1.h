90e652a1fbc68254df31abd534967835a0166e17
wangyingbo <wangyingbo528@126.com>
2017-04-12 18:34:29 +0800

    test1 commit

---
+++ testReset/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  testReset
+//
+//  Created by 王迎博 on 2017/4/12.
+//  Copyright © 2017年 edianzu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+@end
---
+++ testReset/Test1.m
@@ -0,0 +1,13 @@
+//
+//  Test1.m
+//  testReset
+//
+//  Created by 王迎博 on 2017/4/12.
+//  Copyright © 2017年 edianzu. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+@end
