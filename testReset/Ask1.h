6fe53e9b15c5b284c8dbedff79cbe416a0bb287e
wangyingbo <wangyingbo528@126.com>
2017-04-12 18:36:29 +0800

    ask1 commit

---
+++ testReset/Ask1.h
@@ -0,0 +1,13 @@
+//
+//  Ask1.h
+//  testReset
+//
+//  Created by 王迎博 on 2017/4/12.
+//  Copyright © 2017年 edianzu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ask1 : NSObject
+
+@end
---
+++ testReset/Ask1.m
@@ -0,0 +1,13 @@
+//
+//  Ask1.m
+//  testReset
+//
+//  Created by 王迎博 on 2017/4/12.
+//  Copyright © 2017年 edianzu. All rights reserved.
+//
+
+#import "Ask1.h"
+
+@implementation Ask1
+
+@end
