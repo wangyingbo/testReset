2ebf06aedf709ad5e4d9c28617d74383af75c8e8
wangyingbo <wangyingbo528@126.com>
2017-04-14 15:35:12 +0800

    fool1

---
+++ testReset/Fool1.h
@@ -0,0 +1,13 @@
+//
+//  Fool1.h
+//  testReset
+//
+//  Created by 王迎博 on 2017/4/14.
+//  Copyright © 2017年 edianzu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fool1 : NSObject
+
+@end
---
+++ testReset/Fool1.m
@@ -0,0 +1,13 @@
+//
+//  Fool1.m
+//  testReset
+//
+//  Created by 王迎博 on 2017/4/14.
+//  Copyright © 2017年 edianzu. All rights reserved.
+//
+
+#import "Fool1.h"
+
+@implementation Fool1
+
+@end
